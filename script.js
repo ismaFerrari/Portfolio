537ad487f3d50d5e4391d4580bc933178877e67d
ismaFerrari <ismaelferrari97@gmail.com>
2023-05-06 17:33:47 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('Contract-form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const Message = document.getElementById('message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Aquí puedes agregar código para enviar el formulario o mostrar un mensaje de confirmación al usuario.
+});
